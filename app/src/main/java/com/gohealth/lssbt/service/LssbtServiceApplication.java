b27e5ae53a8446a149a351434be80f05672d5223
ihor.bondarenko <ihor.bondarenko@slovenskoit.sk>
2024-06-17 17:34:07 +0200

    Added project skeleton

---
+++ app/src/main/java/com/gohealth/lssbt/service/LssbtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gohealth.lssbt.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssbtServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LssbtServiceApplication.class, args);
+  }
+}
---
+++ app/src/test/java/com/gohealth/lssbt/service/LssbtServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gohealth.lssbt.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LssbtServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
